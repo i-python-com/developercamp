e74ea522404cae67712de802d08bcdf63d159dbb
i-python-com <wyp4wj@gmail.com>
2019-09-14 18:12:53 +0800

    Put return in all the catch function in routes/api/auth.js, user.sj

--- routes/api/users.js
+++ routes/api/users.js
@@ -82,7 +82,7 @@ router.post(
       )
     } catch (err) {
       console.error(err.message)
-      res.status(500).send('Server error')
+      return res.status(500).send('Server error')
     }
   }
 )
