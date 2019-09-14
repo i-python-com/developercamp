ed65611507d0576acfc8772fff30fcf8c4b9e708
i-python-com <wyp4wj@gmail.com>
2019-09-14 16:15:11 +0800

    Implement DELETE api/profile route, to delete profile and user by token

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -146,4 +146,22 @@ router.get('/user/:user_id', async (req, res) => {
   }
 })
 
+// @route   DELETE api/profile
+// @desc    Delete profile, user & posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove users posts
+
+    // Remove profile
+    await Profile.findOneAndDelete({ user: req.user.id })
+
+    await User.findOneAndDelete({ _id: req.user.id })
+    res.json({ msg: 'User deleted' })
+  } catch (err) {
+    console.error(err.message)
+    return res.status(500).send('Server Error')
+  }
+})
+
 module.exports = router
