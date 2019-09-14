48ca185a10185d8673ed0b1100bd0c7023784489
i-python-com <wyp4wj@gmail.com>
2019-09-14 15:33:37 +0800

    Implement GET api/profile route and GET api/profile/user/:user_id
    api/profile get all profiles, api/profile/user/:user_id get profile by
    user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -24,7 +24,7 @@ router.get('/me', auth, async (req, res) => {
     res.json(profile)
   } catch (err) {
     console.error(err.message)
-    res.status(500).send('Server Error')
+    return res.status(500).send('Server Error')
   }
 })
 
@@ -108,9 +108,42 @@ router.post(
       res.json(profile)
     } catch (err) {
       console.error(err.message)
-      res.status(500).send('Server Error')
+      return res.status(500).send('Server Error')
     }
   }
 )
+
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar'])
+    res.json(profiles)
+  } catch (err) {
+    console.error(err.message)
+    return res.status(500).send('Server Error')
+  }
+})
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'avatar'])
+
+    if (!profile) return res.status(400).json({ msg: 'Profile not found' })
+    res.json(profile)
+  } catch (err) {
+    console.error(err.message)
+    if (err.kind === 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' })
+    }
+    return res.status(500).send('Server Error')
+  }
+})
 
 module.exports = router
