6fd1c861d8dcc1cdf96c1828773120eaf8f5dc34
i-python-com <wyp4wj@gmail.com>
2019-09-16 01:13:17 +0800

    Implement loadUser action to authorize the user when App mounted

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
@@ -8,10 +8,22 @@ import Alert from './components/layout/Alert'
 // Redux
 import { Provider } from 'react-redux'
 import store from './store'
+import { loadUser } from './actions/auth'
+import setAuthToken from './utils/setAuthToken'
 
 import './App.css'
 
+// Set every time when webpage refresh
+if (localStorage.token) {
+  setAuthToken(localStorage.token)
+}
+
 const App = () => {
+  useEffect(() => {
+    // When App mounted, dispath the action loadUser
+    store.dispatch(loadUser())
+  }, []) // If no [], dispatch every time when state change
+
   return (
     <Provider store={store}>
       <Router>
--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,33 @@
 import axios from 'axios'
 import { setAlert } from './alert'
-import { REGISTER_SUCCESS, REGISTER_FAIL } from '../actions/types'
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR
+} from '../actions/types'
+import setAuthToken from '../utils/setAuthToken'
+
+// Load user
+export const loadUser = () => async dispatch => {
+  if (localStorage.token) {
+    // when dispatch loadUser action, run setAuthToken
+    setAuthToken(localStorage.token)
+  }
+
+  try {
+    const res = await axios.get('/api/auth')
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data
+    })
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR
+    })
+  }
+}
 
 // Register user
 export const register = ({ name, email, password }) => async dispatch => {
