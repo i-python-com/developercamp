3025b350a1e5be4ae5c4b55bc70b470c4647c11c
i-python-com <wyp4wj@gmail.com>
2019-09-15 12:31:25 +0800

    Implement GET api/posts, GET api/posts/:id, DELETE api/posts/:id
    Get all posts, get post by id, delete post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -46,4 +46,67 @@ router.post(
   }
 )
 
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 }) // new one at top
+    res.json(posts)
+  } catch (err) {
+    console.error(err.message)
+    return res.status(500).send('Server Error')
+  }
+})
+
+// @route   GET api/posts/:id
+// @desc    Get post by Id
+// @access  Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    // post not exist
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+
+    res.json(post)
+  } catch (err) {
+    console.error(err.message)
+    // post id in req.parmas not exist
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+    return res.status(500).send('Server Error')
+  }
+})
+
+// @route   DELETE api/posts/:id
+// @desc    Delete a post
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+
+    // check user, type of post.user is ObjectId, need change to string
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User is not authorized' })
+    }
+
+    await post.remove()
+    res.json({ msg: 'Post removed' })
+  } catch (err) {
+    console.error(err.message)
+    // post id in req.parmas not exist
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+    return res.status(500).send('Server Error')
+  }
+})
+
 module.exports = router
