e51269104179456d861ea0e9e126deb57c90de57
i-python-com <wyp4wj@gmail.com>
2019-09-16 00:29:44 +0800

    Implement utils function setAuthToken
    When we have a token in localStorage, send it at every request

---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+// When we have a token,  send it at every request
+const setAuthToken = token => {
+  if (token) {
+    axios.defaults.headers.common['x-auth-token'] = token
+  } else {
+    delete axios.defaults.headers.common['x-auth-token']
+  }
+}
+
+export default setAuthToken
