e8ec252ab8edd44168bbbfb220ab490945e34b74
i-python-com <wyp4wj@gmail.com>
2019-09-15 23:06:13 +0800

    Implement Alert component, setAlert action, and alert reducer

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -3,10 +3,14 @@ import { SET_ALERT, REMOVE_ALERT } from './types'
 
 // setAlert function return dispatch function
 // can execute multiple dispatch action in setAlert()
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
   const id = uuid.v4()
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id }
   })
+
+  setTimeout(() => {
+    dispatch({ type: REMOVE_ALERT, payload: id })
+  }, timeout)
 }
