1b07d6d6eb10a441b392ca2fc75460e6a35abfc6
i-python-com <wyp4wj@gmail.com>
2019-09-17 00:29:40 +0800

    Set axios.defaults.baseURL in client/index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://localhost:5000'
 
 ReactDOM.render(<App />, document.getElementById('root'))
