d48f40ef541b4aba0a0522a897f2c9e9e6d83cdc
i-python-com <wyp4wj@gmail.com>
2019-09-22 18:03:07 +0800

    Create getProfiles, getProfileById, getGithubRepos action
    Update types and profile reducers

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,8 +1,10 @@
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_ERROR,
   CLEAR_PROFILE,
-  UPDATE_PROFILE
+  UPDATE_PROFILE,
+  GET_REPOS
 } from '../actions/types'
 
 const initialState = {
@@ -25,6 +27,13 @@ export default (state = initialState, action) => {
         loading: false
       }
 
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false
+      }
+
     case PROFILE_ERROR:
       return {
         ...state,
@@ -37,6 +46,13 @@ export default (state = initialState, action) => {
         ...state,
         profile: null,
         repos: [],
+        loading: false
+      }
+
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
         loading: false
       }
 
