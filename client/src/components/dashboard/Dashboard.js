258994160fc17e899de32d4cdf65bbe8f2c7ff3f
i-python-com <wyp4wj@gmail.com>
2019-09-22 16:11:36 +0800

    Add Experience, Education component to Dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
 import DashboardActions from '../dashboard/DashboardActions'
+import Experience from '../dashboard/Experience'
+import Education from '../dashboard/Education'
 import { getCurrentProfile } from '../../actions/profile'
 
 const Dashboard = ({
@@ -26,6 +28,8 @@ const Dashboard = ({
       {profile !== null ? (
         <Fragment>
           <DashboardActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
         </Fragment>
       ) : (
         <Fragment>
