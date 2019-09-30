0585229221793aef3cbe705908954c2d69686ec4
i-python-com <wyp4wj@gmail.com>
2019-09-30 18:09:11 +0800

    Implement PostForm component, addPost action to add a post

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -2,7 +2,8 @@ import {
   GET_POSTS,
   POST_ERROR,
   UPDATE_LIKES,
-  DELETE_POST
+  DELETE_POST,
+  ADD_POST
 } from '../actions/types'
 
 const initialState = {
@@ -18,6 +19,13 @@ export default (state = initialState, action) => {
   switch (type) {
     case GET_POSTS:
       return { ...state, posts: payload, loading: false }
+
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [payload, ...state.posts],
+        loading: false
+      }
 
     case DELETE_POST:
       return {
