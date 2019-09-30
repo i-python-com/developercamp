baa629ea9db72a79135bca4fe59b7be1e813e701
i-python-com <wyp4wj@gmail.com>
2019-09-30 19:05:43 +0800

    Implement CommentForm compo, addComment action to add a comment

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -6,7 +6,9 @@ import {
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
-  ADD_POST
+  ADD_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT
 } from './types'
 
 // Get posts
@@ -117,6 +119,44 @@ export const getPost = id => async dispatch => {
         msg: err.response.statusText,
         status: err.response.status
       }
+    })
+  }
+}
+
+// Add comment
+export const addComment = (postId, formData) => async dispatch => {
+  try {
+    const res = await axios.post(`/api/posts/comment/${postId}`, formData)
+
+    dispatch({
+      type: ADD_COMMENT,
+      payload: res.data
+    })
+
+    dispatch(setAlert('Comment Added', 'success'))
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    })
+  }
+}
+
+// Delete comment
+export const deleteComment = (postId, commentId) => async dispatch => {
+  try {
+    await axios.delete(`/api/posts/comment/${postId}/${commentId}`)
+
+    dispatch({
+      type: REMOVE_COMMENT,
+      payload: commentId
+    })
+
+    dispatch(setAlert('Comment Removed', 'success'))
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
     })
   }
 }
